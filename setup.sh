5bec469034209e69e0228874e07bc00a919bb4dc
William Austin Rhodes <wrhodes2@hydra17.eecs.utk.edu>
2021-11-22 15:45:15 -0500

    reorganized everything

--- setup.sh
+++
@@ -1,6 +0,0 @@
-rm ~/.config/yed/yedrc
-rm ~/.config/yed/ypm_list
-ln -s ~/dotfiles/.config/yed/yedrc ~/.config/yed/yedrc
-ln -s ~/dotfiles/.config/yed/ypm_list ~/.config/yed/ypm_list
-mkdir ~/.config/yed/my_styles
-ln -s ~/dotfiles/.config/yed/my_styles/bourbon.fstyle ~/.config/yed/my_styles/bourbon.fstyle
---
+++ setup_yed.sh
@@ -0,0 +1,7 @@
+rm ~/.config/yed/yedrc
+rm ~/.config/yed/ypm_list
+rm ~/.config/yed/my_styles/bourbon.fstyle
+ln -s ~/dotfiles/yed/yedrc ~/.config/yed/yedrc
+ln -s ~/dotfiles/yed/ypm_list ~/.config/yed/ypm_list
+mkdir ~/.config/yed/my_styles
+ln -s ~/dotfiles/yed/my_styles/bourbon.fstyle ~/.config/yed/my_styles/bourbon.fstyle
