431d33acb9f1e30ed80d367385ed1eb0dedb45b4
Austin Rhodes <warhodes@austins-mbp.lan>
2021-12-10 17:23:40 -0500

    removed annoying shit from zshrc

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm ~/.config/nvim/init.vim
 rm ~/.vim/autoload/plug.vim
+mkdir -p ~/.config/nvim
+mkdir -p ~/.vim/autoload
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/nvim/plug.vim ~/.vim/autoload/plug.vim
