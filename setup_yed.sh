11557664b4a762506e372a072aaf21f71398bda1
Austin <wrhodes2@com1634.eecs.utk.edu>
2022-02-03 10:32:38 -0500

    affected style

--- setup_yed.sh
+++ setup_yed.sh
@@ -6,3 +6,5 @@ ln -s ~/dotfiles/yed/yedrc ~/.config/yed/yedrc
 ln -s ~/dotfiles/yed/ypm_list ~/.config/yed/ypm_list
 mkdir ~/.config/yed/my_styles
 ln -s ~/dotfiles/yed/my_styles/bourbon.fstyle ~/.config/yed/my_styles/bourbon.fstyle
+mkdir ~/.config/yed/templates
+ln -s ~/dotfiles/yed/templates/yedrc.txt ~/.config/yed/templates/yedrc.txt
